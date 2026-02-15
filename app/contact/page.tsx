385804aac70e8c0970561249324a42b58b7f2775
Tabibur Rahman <your-email@example.com>
2026-02-15 20:25:48 +0600

    feat: contact page added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,51 +1,27 @@
-"use client";
-
 import { ContactCTA } from "@/components/ContactCTA";
 import { ContactForm } from "@/components/ContactForm";
 import { ContactHero } from "@/components/ContactHero";
 import { ContactInformation } from "@/components/ContactInformation";
-import { Footer } from "@/components/Footer";
-import { MainHeader } from "@/components/MainHeader";
+
 import { OurOffice } from "@/components/OurOffice";
-import { TopBar } from "@/components/TopBar";
-import { useEffect, useState } from "react";
+
 const ContactPage = () => {
-  //   const [isScrolled, setIsScrolled] = useState(false);
-
-  //   useEffect(() => {
-  //     const handleScroll = () => {
-  //       setIsScrolled(window.scrollY > 20);
-  //     };
-
-  //     window.addEventListener('scroll', handleScroll);
-  //     return () => window.removeEventListener('scroll', handleScroll);
-  //   }, []);
-
   return (
     <div className="min-h-screen bg-white">
-      {/* Top Bar */}
-      <TopBar />
-
-      {/* Main Header */}
-      {/* <MainHeader isScrolled={isScrolled} /> */}
-
       {/* Contact Hero Section */}
-      {/* <ContactHero /> */}
+      <ContactHero />
 
       {/* Contact Information Section */}
-      {/* <ContactInformation /> */}
+      <ContactInformation />
 
       {/* Send Us a Message Form */}
-      {/* <ContactForm /> */}
+      <ContactForm />
 
       {/* Our Office Section */}
-      {/* <OurOffice /> */}
+      <OurOffice />
 
       {/* Final CTA Section */}
-      {/* <ContactCTA /> */}
-
-      {/* Footer */}
-      {/* <Footer /> */}
+      <ContactCTA />
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/lib/Providers";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "OptixPathway - Your Trusted Pathway to Study in the UK",
@@ -27,9 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={` antialiased`}>
-        <Providers>
-        {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
